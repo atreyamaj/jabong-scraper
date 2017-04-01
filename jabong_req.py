839f37a6e28fd8c09564bd59fadd78008524e4eb
salman-bhai <sbs.191197@gmail.com>
2017-04-01 15:38:41 +0530

    Final code changes

--- jabong_req.py
+++ jabong_req.py
@@ -6,15 +6,15 @@ import csv
 import time
 
 count = 0
-process = 0
 start_time = time.time()
+process = 0
 
 with open('jabong.csv', 'w') as csvfile:
     jabongwriter = csv.writer(csvfile, delimiter=',')
     jabongwriter.writerow(['ID', 'Title', 'Original Price', 'Discounted Price', 'URL'])
     csvfile.flush()
     for i in range(1,300):
-        r = requests.get('http://www.jabong.com/men/clothing/polos-tshirts/?sort=popularity&dir=desc&source=topnav_men&page='+str(i))
+        r = requests.get('http://www.jabong.com/men/clothing/polos-tshirts/?sort=popularity&dir=desc&source=topnav_men&limit=1015&page='+str(i))
         data = r.text
         soup = BeautifulSoup(data, "lxml")
 
@@ -22,13 +22,10 @@ with open('jabong.csv', 'w') as csvfile:
 
         products = soup.find_all("div", class_="product-info")
         link = "http://www.jabong.com/men/clothing/polos-tshirts/"
-
-        process_start = time.time()
         
         for product in products:
 
             title = product.find('div', class_='h4')
-#     id        print title.text
             title_list = title.text
             title_list = unicode(title_list).encode('utf-8')
 
@@ -38,27 +35,22 @@ with open('jabong.csv', 'w') as csvfile:
             mrp_price = ""
             discounted_price = "" 
             if len(standard_price) != 0:
-#                 print standard_price[0].string
 				mrp_price = standard_price[0].string
 				mrp_price = unicode(mrp_price).encode('utf-8')
 				if len(standard_price) == 2:
-#                     print standard_price[1].string
 					discounted_price = standard_price[1].string
 					discounted_price = unicode(discounted_price).encode('utf-8')
 				else:
-				#                     print "None"
 				    discounted_price = standard_price[0].string
 				    discounted_price = unicode(discounted_price).encode('utf-8')
 				count += 1		
 
-#             print(link + str(product.parent.get('href')))
 				url_link = link + str(product.parent.get('href'))
 				url_link = unicode(url_link).encode('utf-8')
-				# print("count = " + str(count))
+				print("count = " + str(count))
 
 				id_item = product.parent.parent.get('data-product-id')
 				id_item = unicode(id_item).encode('utf-8')
-#             print(str(id_item))
             
 				if id_item == None:
 				    continue
@@ -66,15 +58,13 @@ with open('jabong.csv', 'w') as csvfile:
 				    jabongwriter.writerow([id_item, title_list, mrp_price, discounted_price, url_link])
 				    csvfile.flush()
 
-				process_end = time.time()
 
 				if count == 10000:
 				    end_time = time.time()
 				    print end_time-start_time
 				    sys.exit(0)
-
-		process += process_end - process_start
-        print("Process time:" + str(process_end - process_start))
+				else:
+					continue
 
 end_time = time.time()
 print("Total of " + str(count) + "items taken")
